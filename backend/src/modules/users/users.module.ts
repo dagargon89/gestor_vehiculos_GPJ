e8e0c35acd05f4ce3a69cfc6e21fa28564f5cc5a
dagargon89 <dagargon89@gmail.com>
2026-02-09 13:50:11 -0600

    Update server listening address to '0.0.0.0' and include Permission entity in Users module imports

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../../database/entities/user.entity';
 import { Role } from '../../database/entities/role.entity';
+import { Permission } from '../../database/entities/permission.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role])],
+  imports: [TypeOrmModule.forFeature([User, Role, Permission])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
